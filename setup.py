a1457b83598e9295de6d62e16b31b67e445c5243
Faye.Nielsen <faye@nicholsonconsulting.co.nz>
2021-12-22 15:01:08 +1300

    update setup files

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+    name = "Calculator",
+    packages = ["Calculator"],
+    version = "0.0.1",
+    author = "Faye Nielsen",
+    author_email = "fayemnielsen@gmail.com",
+    description = "Basic calculator functionality",
+    url = "https://github.com/fayenielsen/Calculator",
+    download_url = "https://github.com/fayenielsen/Calculator/archive/refs/tags/0.0.1.tar.gz",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = ">=3.9",
+    include_package_data = "True",
+)
